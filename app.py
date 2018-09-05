74e4d4d44b2633d1d32de4e3203aa0d4415010b4
Vincent <vincentmuriuki42@gmail.com>
2018-09-05 03:15:59 +0300

    Added GET API endpoint to retrieve all orders

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "")
 
 
 orders = [
@@ -38,6 +38,26 @@ orders = [
     }
 ]
 
+def make_public_order(order):
+    new_order = {}
+    for field in order:
+        if field == 'id':
+            new_order['uri'] = url_for('get_order', order_id = order['id'], _external = True)
+        else:
+            new_order[field] = order[field]
+    return new_order
+
+
+@app.route('/api/v1/orders', methods = ['GET'])
+def get_orders():
+    return jsonify({'orders': list(map(make_public_order, orders))})
+
+@app.route('/api/orders/<int:order_id>', methods = ['GET'])
+def get_order(order_id):
+    order = filter(lambda t: t['id'] == order_id, orders)
+    if len(order) == 0:
+        abort(404)
+    return jsonify( { 'order': make_public_order(order[0]) })
 
 
 if __name__ == '__main__':
